234c52330e27c672dafab522c3232023c4f395ed
ibrahem430 <habboushebraheem@gmail.com>
2025-06-14 22:14:17 +0300

    f1

---
+++ project_smartStore/src/index.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+// function Card(props) {
+//   return (
+//     <div style={{margin:"0px"}}
+//     className="theCrde">
+      
+//       <img className="size_photo" src={props.img} alt="avatar_img" />
+//       <h2>{props.name}</h2>
+//       <p>{props.tel}</p>
+//       <p>{props.email}</p>
+//     </div>
+//   );
+// }
+
+ReactDOM.render(
+ <div>
+  <App/>
+  </div>,
+  document.getElementById("root")
+);
